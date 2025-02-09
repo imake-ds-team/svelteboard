b6106cfc6e8670ca40651159828f8ef3e6be495d
imake-ds-team <ugreenridg2@student.rccd.edu>
2025-02-08 18:15:17 -0800

    Rules page

---
+++ src/routes/rules/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    // @ts-nocheck
+    import type { PageProps } from "./$types";
+    let { data }: PageProps = $props();
+</script>
+
+<svelte:head>
+    <title>{data.global_name}</title>
+</svelte:head>
+
+<div class="flex h-screen">
+    <div class="container container-sm m-auto px-10 sm:p-5 text-center">
+        <h1 class="text-3xl">{data.global_name}</h1>
+        <p>{data.global_description}</p>
+        <table class="table-auto border-spacing-x-5 w-full">
+            <thead>
+                <tr>
+                    <th>Rules</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        Your rules go here...
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
